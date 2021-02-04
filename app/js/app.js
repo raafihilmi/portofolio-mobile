c303e70018467e602ea99d368ce376004590d3cf
Raafi Hilmi <raafihilmi90@gmail.com>
2021-02-04 15:08:14 +0700

    update modal and card

--- app/js/app.js
+++ app/js/app.js
@@ -5,3 +5,9 @@ const links = document.querySelector('.nav__links');
 navToggle.addEventListener('click', function () {
   links.classList.toggle('nav__show');
 });
+
+// Modal
+function onClick(element) {
+  document.getElementById('img01').src = element.src;
+  document.getElementById('modal01').style.display = 'block';
+}
